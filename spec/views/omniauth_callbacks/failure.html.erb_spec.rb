c73663cbd6bd1a1f780c9bceb7526c289f12a6b8
Loïc Guitaut <loic@discourse.org>
2022-02-02 16:32:03 +0100

    WIP: fix template spec

--- spec/views/omniauth_callbacks/failure.html.erb_spec.rb
+++ spec/views/omniauth_callbacks/failure.html.erb_spec.rb
@@ -3,12 +3,10 @@
 require "rails_helper"
 
 describe "users/omniauth_callbacks/failure.html.erb" do
-
   it "renders the failure page" do
     flash[:error] = I18n.t("login.omniauth_error", strategy: 'test')
-      render
+    render template: 'users/omniauth_callbacks/failure'
 
-      expect(rendered.match(I18n.t("login.omniauth_error.generic", strategy: 'test'))).not_to eq(nil)
+    expect(rendered.match(I18n.t("login.omniauth_error.generic", strategy: 'test'))).not_to eq(nil)
   end
-
 end
